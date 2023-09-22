74314f9b7b2526f98985c5998676012dec67a60a
Malcolm Harrow <harrow.malcolm@gmail.com>
2023-09-22 18:13:21 -0400

    Tidy up directory

--- src/view.c
+++ src/view.c
@@ -57,29 +57,17 @@ int main() {
         goto exit;
     }
 
-    //  Bytes per sector. Allowed values include 512, 1024, 2048, and 4096
-    // uint16_t bps = boot_entry.sector_size[0] + ((uint16_t) boot_entry.sector_size[1] << 8);
-    // off_t root_entry_offset = ( boot_entry.reserved +
-    //                             boot_entry.fats * boot_entry.fat32.length) * bps;
-    // uint32_t bpc = bps * boot_entry.sec_per_clus;
-    // off_t fat_offset = bps * boot_entry.reserved;
 
-    printf("sector size %d\n", boot_entry.sector_size);
+    uint32_t sector_size = (boot_entry.sector_size[0]) + (boot_entry.sector_size[1]<<8); 
+    printf("sector size %d\n", sector_size);
     printf("reserved %d\n", boot_entry.reserved);
     printf("sectors per cluster %d\n", boot_entry.sec_per_clus);
     printf("root directory cluster %d\n", boot_entry.fat32.root_cluster);
     printf("number of fat tables %d\n", boot_entry.fats);
-    uint16_t rootDirectorySector = boot_entry.fat32.root_cluster * boot_entry.fats + boot_entry.reserved;
+    uint16_t rootDirectorySector = boot_entry.fat_length * boot_entry.fats + boot_entry.reserved;
     printf("(calc) rootDirectorySector %d\n", rootDirectorySector);
-    
-//     unsigned int i, start_sector, length_sectors;
-//     Parttition tables for primary partition
-// 0x1BE 
-// 0x1CE
-// 0x1DE
-// 0x1EE
 
-    // tidy this uop with a better MBR structure ..
+    // tidy this up with a better MBR structure ..
 
     struct msdos_partition part[4];
     
@@ -90,50 +78,22 @@ int main() {
     for (int i=0; i<4; i++) {  
         printf("%02X ", part[i].bootable);
         printf("%02X ", part[i].partition_type);
-        printf("%08X\n", part[i].number_sectors);
+        printf("%08X   ", part[i].number_sectors);
     }
+    printf("\n");
     
     DumpHex(0x1BE, &part, sizeof(struct msdos_partition)*4);
-
     printf("\n");
+
+
     // print out directory table
-    fseek(fp, rootDirectorySector * boot_entry.sec_per_clus * boot_entry.sector_size, SEEK_SET); // go to partition table start
+    fseek(fp, rootDirectorySector * boot_entry.sec_per_clus * sector_size, SEEK_SET); // go to partition table start
 
     struct msdos_dir_entry dir_entry[16];
-    size_t ret = fread(&dir_entry, rootDirectorySector * boot_entry.sec_per_clus * boot_entry.sector_size, 16*sizeof(struct msdos_dir_entry), fp);
-//               
-    DumpHex(rootDirectorySector * boot_entry.sec_per_clus * boot_entry.sector_size, &dir_entry, 16*sizeof(struct msdos_dir_entry));
+    res = fread(&dir_entry, rootDirectorySector * sector_size, 16*sizeof(struct msdos_dir_entry), fp);
 
+    DumpHex(rootDirectorySector * sector_size, &dir_entry, 16*sizeof(struct msdos_dir_entry));
 
-
-    
-// for(uint32_t i = 0; i < bpc / sizeof(struct msdos_dir_entry); i++) {
-//               struct msdos_dir_entry dir_entry;
-//               size_t ret = fread(&dir_entry, 1, sizeof(struct msdos_dir_entry), fp);
-//               if(dir_entry.name[0] != 0x00) {
-//                   if(dir_entry.attr != 0x0f) {
-                    // Insert your code here
-                    /*
-                    Read the file name:
-                      1. Check whether the file is deleted or not
-                      2. Convert to 8.3 format names
-                      3. Check whether it is a subdirectory
-
-                    uint32_t dir_first_cluster = get_dir_first_cluster(&dir_entry);
-                    printf("%d, ", _);
-                    printf("%s, ", _);
-                    printf("%d, ", _);
-                    printf("%d", _);
-                    printf("\n");
-                    */
-                //   }
-                //   if((dir_entry.attr & 0x10) != 0) {
-  /* It is a subdirectory */
-//  }
-            //       else {
-            //           printf("%d, LFN entry\n", idx++);
-            //       }
-            //   }
     return 0;
 exit:
     return 1;
